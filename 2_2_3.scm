30718f1887434202880863731e3a6d407222da5a
hongweizhuo <hzhuo@indeed.com>
2020-04-17 19:44:20 +0800

    2.2.3 WIP

--- 2_2_3.scm
+++ 2_2_3.scm
@@ -6,10 +6,10 @@
           (accumulate op initial (cdr sequence)))))
 
 ;ex 2.33
-(define (map p sequence)
+(define (map-alt p sequence)
   (accumulate (lambda (x y) (cons (p x) y)) nil sequence))
 
-(map (lambda (x) (* x x)) (list 1 2 3 4 5))
+(map-alt (lambda (x) (* x x)) (list 1 2 3 4 5))
 
 (define (append seq1 seq2)
   (accumulate cons seq2 seq1))
@@ -20,3 +20,28 @@
   (accumulate (lambda (x y) (+ 1 y)) 0 sequence))
 
 (length (list 1 2 3))
+
+;ex 2.35
+(define (count-leaves t)
+  (accumulate + 0 (map (lambda (x)
+                         (cond ((null? x) 0)
+                               ((not (pair? x)) 1)
+                               (else (count-leaves x)))) t)))
+
+(count-leaves (cons (list 1 2) (list 3 4)))
+
+;ex 2.36
+(define (accumulate-n op init seqs)
+  (if (null? (car seqs)) nil
+      (cons (accumulate op init (map car seqs))
+            (accumulate-n op init (map cdr seqs)))))
+
+(accumulate-n + 0 (list (list 1 2 3) (list 4 5 6)))
+
+;ex 2.37
+(define (dot-product v w)
+      (accumulate + 0 (map * v w)))
+
+(dot-product '(1 2 3) '(4 5 6))
+
+
