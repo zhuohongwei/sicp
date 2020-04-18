3ec7347927e68825ef059d6812cf3d9a073860e2
Hong Wei <hongwei.zhuo@gmail.com>
2020-04-18 11:56:20 +0800

    2.2.3 WIP

--- 2_2_3.scm
+++ 2_2_3.scm
@@ -60,3 +60,73 @@
 
 (matrix-*-matrix (list (list 0 3 5) (list 5 5 2)) (list (list 3 4) (list 3 -2) (list 4 -2)))
 
+;ex 2.38
+(define fold-right accumulate)
+(define (fold-left op initial sequence)
+  (define (iter result rest)
+    (if (null? rest) result
+        (iter (op result (car rest))
+              (cdr rest))))
+  (iter initial sequence))
+
+(fold-right / 1 (list 1 2 3))
+(fold-left / 1 (list 1 2 3))
+(fold-right list nil (list 1 2 3))
+(fold-left list nil (list 1 2 3))
+
+;in order for fold-right and fold-left to produce the same results,
+;op must be associate (x * y) * z == x * (y * z)
+;for example,
+(fold-right * 1 (list 1 2 3))
+(fold-left * 1 (list 1 2 3))
+
+;ex 2.39
+(define (reverse sequence)
+  (fold-right (lambda (x y) (append y (list x))) nil sequence))
+
+(reverse (list 1 2 3 4))
+
+(define (reverse-alt sequence)
+  (fold-left (lambda (x y) (append (list y) x)) nil sequence))
+
+(reverse-alt (list 1 2 3 4))
+
+
+;ex 2.40
+(define (flatmap proc seq)
+  (accumulate append nil (map proc seq)))
+
+(define (enumerate-interval a b)
+  (if (> a b)
+      nil
+      (cons a (enumerate-interval (+ a 1) b))))
+
+(enumerate-interval 1 10)
+
+(define (unique-pairs n)
+  (flatmap (lambda (i)
+             (map (lambda (j) (list j i))
+                    (enumerate-interval 1 (- i 1))))
+           (enumerate-interval 1 n)))
+
+(unique-pairs 5)
+
+;ex 2.41
+(define (unique-triplets n)
+  (flatmap (lambda (p)
+             (map (lambda (k) (cons k p))
+                  (enumerate-interval 1 (- (car p) 1))))
+           (unique-pairs n)))
+
+(unique-triplets 5)
+
+(define (filter f seq)
+  (cond ((null? seq) nil)
+        ((f (car seq)) (cons (car seq) (filter f (cdr seq))))
+        (else (filter f (cdr seq)))))
+
+(define (triplets-matching-sum s n)
+  (filter (lambda (t) (= (accumulate + 0 t) s)) (unique-triplets n)))
+
+(triplets-matching-sum 10 5)
+
