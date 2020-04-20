bbbee0dc9bdedeba7e7c0a707557a0a8c3697c66
Hong Wei <hongwei.zhuo@gmail.com>
2020-04-20 22:37:41 +0800

    2.2.4 WIP

--- 2_2_4.scm
+++ 2_2_4.scm
@@ -109,7 +109,6 @@
                         (scale-vect (ycor-vect v) (edge2-frame frame))))))
 
 ;ex 2.48
-
 (define (make-segment start end)
   (cons start end))
 
@@ -183,3 +182,60 @@
                         (make-segment w n)))))
 
 (draw-diamond some-frame)
+
+;ex 2.50
+(define (transform-painter painter origin corner1 corner2)
+  (lambda (frame)
+    (let ((m (frame-coord-map frame)))
+      (let ((new-origin (m origin)))
+        (painter (make-frame new-origin
+                             (sub-vect (m corner1) new-origin)
+                             (sub-vect (m corner2) new-origin)))))))
+
+(define (beside-alt painter1 painter2)
+  (let ((split-point (make-vect 0.5 0.0)))
+    (let ((paint-left (transform-painter
+                       painter1
+                       (make-vect 0.0 0.0)
+                       split-point
+                       (make-vect 0.0 1.0)))
+          (paint-right (transform-painter
+                        painter2
+                        split-point
+                        (make-vect 1.0 0.0)
+                        (make-vect 0.5 1.0))))
+      (lambda (frame)
+        (paint-left frame)
+        (paint-right frame)))))
+
+(define (flip-horiz-alt painter)
+  (transform-painter painter (make-vect 1 0) (make-vect 0 0) (make-vect 1 1)))
+
+(define (rotate90 painter)
+  (transform-painter painter (make-vect 1.0 0.0) (make-vect 1.0 1.0) (make-vect 0.0 0.0)))
+
+(define (rotate180 painter)
+  (transform-painter painter (make-vect 1 1) (make-vect 0 1) (make-vect 1 0)))
+
+(define (rotate270 painter)
+  (transform-painter painter (make-vect 0 1) (make-vect 0 0) (make-vect 1 1)))
+
+;ex 2.51
+(define (below-alt painter1 painter2)
+  (let ((split-point (make-vect 0.0 0.5)))
+    (let ((paint-bottom (transform-painter
+                         painter1
+                         (make-vect 0.0 0.0)
+                         (make-vect 1.0 0.0)
+                         split-point))
+          (paint-top (transform-painter
+                      painter2
+                      split-point
+                      (make-vect 1.0 0.0)
+                      (make-vect 0.0 1.0))))
+      (lambda (frame)
+        (paint-bottom frame)
+        (paint-top frame)))))
+
+(define (below-alt-two painter1 painter2)
+  (rotate90 (beside-alt (rotate270 painter1) (rotate270 painter2))))
