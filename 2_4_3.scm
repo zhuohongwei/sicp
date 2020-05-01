2b56f761135949fd02591439f78efb7e45b85503
Hong Wei <hongwei.zhuo@gmail.com>
2020-05-01 14:56:15 +0800

    2.4.3

--- 2_4_3.scm
+++ 2_4_3.scm
@@ -121,5 +121,15 @@
             (find-employee-record (cdr division-files) name)))))
 
 
+; ex 2.75
+
+(define (make-from-mag-ang x y)
+  (define (dispatch op)
+    (cond ((eq? op 'real-part) (* x (cos y)))
+          ((eq? op 'imag-part) (* x (sin y)))
+          ((eq? op 'magnitude) x)
+          ((eq? op 'angle) y)
+          (else (error "Unknown op: MAKE-FROM-MAG-ANG" op))))
+  dispatch)
 
     
