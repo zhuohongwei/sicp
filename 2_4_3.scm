e2d1498a3d42284b2f0eb159363ed48111923867
Hong Wei <hongwei.zhuo@gmail.com>
2020-05-01 13:49:39 +0800

    2.4.3 WIP

--- 2_4_3.scm
+++ 2_4_3.scm
@@ -46,7 +46,7 @@
   (put 'deriv 'make-product make-product)
   'done)
 
-;c
+; c
 (define (install-deriv-exponentiation)
   (define base car)
   (define exponent cadr)
@@ -59,4 +59,67 @@
   (put 'deriv '** deriv-exponentiation)
   (put 'deriv 'make-exponentiation make-exponentiation)
   'done)
+
+; ex 2.74
+; a, b
+(define (attach-division division object)
+  (cons division object))
+
+(define (put-division-op op division proc)
+  (error "Unimplemented Operation Put"))
+
+(define (get-division-op op division)
+  (error "Unimplemented Operation Get"))
+ 
+(define (install-sales-division-package)
+  (define (save-record record) (error "Unimplemented Operation Save" record))
+  (define (all-records) (error "Unimplemented Operation"))
+  (define (make-record name address salary)
+    (list name address salary))
+  (define record-name car)
+  (define record-address cadr)
+  (define record-salary caddr)
+  (define (find-record records name)
+    (cond ((null? records) #f)
+          ((= (record-name (car records)) name) (car records))
+          (else (find-record (cdr records) name))))
+  (define (tag object) (attach-division 'sales object))
+  (put-division-op 'add-record 'sales
+       (lambda (name address salary)
+         (let ((record (make-record name address salary)))
+           (save-record record)
+           (tag record))))
+  (put-division-op 'find-record 'sales
+       (lambda (records name)
+         (let ((record (find-record records name)))
+           (tag record))))
+  (put-division-op 'get-name 'sales record-name)
+  (put-division-op 'get-address 'sales record-address)
+  (put-division-op 'get-salary 'sales record-salary)
+  (put-division-op 'get-records-file 'sales
+                   (lambda () (tag (all-records))))
+  'done)
+
+; c
+(define divisions '(sales operations engineering))
+;(define division-files (map (lambda (division) ((get-division-op 'get-records-file division))) divisions))
+
+(define (apply-division-op op . args)
+  (let ((division-object (car args)))
+    (let ((division (car division-object)))
+      (let ((proc (get-division-op op division)))
+        (if proc
+            (apply proc (cons (cdr division-object) (cdr args)))
+            (error "Operation Not Found" op))))))
+
+(define (find-employee-record division-files name)
+  (if (null? division-files)
+      #f
+      (let ((record (apply-division-op 'find-record (car division-files) name)))
+        (if record
+            record
+            (find-employee-record (cdr division-files) name)))))
+
+
+
     
