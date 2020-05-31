fd56336dc5de6163f5483c74eaeb2426438500c4
Hong Wei <hongwei.zhuo@gmail.com>
2020-05-31 15:51:11 +0800

    3.5.1

--- src/chapter3/3_5_1.scm
+++ src/chapter3/3_5_1.scm
@@ -30,3 +30,105 @@
        (apply stream-map
               (cons proc (map stream-cdr argstreams))))))
 
+
+; ex 3.51
+
+(define (display-line x) (newline) (display x))
+
+(define (stream-enumerate-interval low high)
+  (if (> low high)
+      the-empty-stream
+      (cons-stream low (stream-enumerate-interval (+ low 1) high))))
+                  
+(define (show x)
+  (display-line x)
+  x)
+
+(define x
+  (stream-map show
+              (stream-enumerate-interval 0 10)))
+; prints 0
+
+(stream-ref x 5)
+; prints 1
+; prints 2
+; prints 3
+; prints 4
+; prints 5
+
+(stream-ref x 7)
+; prints 6
+; prints 7
+
+; ex 3.52
+
+(define (stream-filter pred stream)
+  (cond ((stream-null? stream)
+         the-empty-stream)
+        ((pred (stream-car stream))
+         (cons-stream (stream-car stream)
+                      (stream-filter pred
+                       (stream-cdr stream))))
+        (else (stream-filter pred (stream-cdr stream)))))
+
+(define (display-stream s)
+  (stream-for-each display-line s))
+
+(define sum 0)
+
+(define (accum x) (set! sum (+ x sum)) sum)
+
+(define seq
+  (stream-map accum
+              (stream-enumerate-interval 1 20)))
+; seq is 1 . promise
+
+(newline)
+(display "Sum:")
+(display-line sum)
+(newline)
+; sum is 1
+
+(define y (stream-filter even? seq))
+; y is 6 . promise
+
+; 1 3 6 . promise
+
+(newline)
+(display "Sum:")
+(display-line sum)
+(newline)
+; sum is 6
+
+(define z
+  (stream-filter (lambda (x) (= (remainder x 5) 0))
+                 seq))
+; 1 3 6 10 . promise
+
+(newline)
+(display "Sum:")
+(display-line sum)
+(newline)
+; sum is 10
+
+(stream-ref y 7)
+; prints 136
+
+; 6 10 15 21 28 36 45 55 66 78 91 105 120 136 . promise 
+
+(newline)
+(display "Sum:")
+(display-line sum)
+(newline)
+; sum is 136
+
+(display-stream z)
+; prints 10 15 45 55 105 120 190 210
+
+; 6 10 15 21 28 36 45 55 66 78 91 105 120 136 153 171 190 210 
+
+(newline)
+(display "Sum:")
+(display-line sum)
+(newline)
+; sum is 210
