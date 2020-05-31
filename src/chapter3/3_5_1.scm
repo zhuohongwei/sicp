159080b70b6dec23e3a760a1369ac761617c76d8
Hong Wei <hongwei.zhuo@gmail.com>
2020-05-31 14:13:13 +0800

    3.5.1 WIP

---
+++ src/chapter3/3_5_1.scm
@@ -0,0 +1,32 @@
+#lang sicp
+
+(define (stream-car stream) (car stream))
+(define (stream-cdr stream) (force (cdr stream)))
+
+(define (stream-ref s n)
+  (if (= n 0)
+      (stream-car s)
+      (stream-ref (stream-cdr s) (- n 1))))
+
+;(define (stream-map proc s)
+ ; (if (stream-null? s)
+  ;    the-empty-stream
+   ;   (cons-stream (proc (stream-car s))
+    ;               (stream-map proc (stream-cdr s)))))
+
+(define (stream-for-each proc s)
+  (if (stream-null? s)
+      'done
+      (begin (proc (stream-car s))
+             (stream-for-each proc (stream-cdr s)))))
+
+; ex 3.50
+
+(define (stream-map proc . argstreams)
+  (if (stream-null? (car argstreams))
+      the-empty-stream
+      (cons-stream
+       (apply proc (map stream-car argstreams))
+       (apply stream-map
+              (cons proc (map stream-cdr argstreams))))))
+
