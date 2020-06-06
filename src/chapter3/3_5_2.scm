acb334f9d5844ff63063d7ddc7cbdd73c39eec84
Hong Wei <hongwei.zhuo@gmail.com>
2020-06-06 16:03:55 +0800

    3.5.2

--- src/chapter3/3_5_2.scm
+++ src/chapter3/3_5_2.scm
@@ -84,3 +84,57 @@
 (define S (cons-stream 1 (merge (scale-stream S 2) (merge (scale-stream S 3) (scale-stream S 5)))))
 
 (display-n-elements 10 S)
+
+; ex 3.58
+
+(define (expand num den radix)
+  (cons-stream
+   (quotient (* num radix) den)
+   (expand (remainder (* num radix) den) den radix)))
+
+(display-n-elements 20 (expand 1 7 10))
+(display-n-elements 20 (expand 3 8 10))
+
+; ex 3.59
+
+;a
+(define (div-streams s1 s2) (stream-map / s1 s2))
+
+(define (integrate-series power-series)
+  (div-streams power-series integers))
+
+(display-n-elements 20 (integrate-series ones))
+
+;b
+
+(define sine-series (cons-stream 0 (integrate-series cosine-series)))
+(define cosine-series (cons-stream 1 (integrate-series (scale-stream sine-series -1))))
+
+; ex 3.60
+
+(define (mul-series s1 s2)
+  (cons-stream
+   (* (stream-car s1) (stream-car s2))
+   (add-streams (scale-stream (stream-cdr s2) (stream-car s1)) (mul-series (stream-cdr s1) s2))))
+
+(define sine-*-sine-plus-cosine-*-cosine (add-streams (mul-series sine-series sine-series) (mul-series cosine-series cosine-series)))
+
+(display-n-elements 1 sine-*-sine-plus-cosine-*-cosine)
+
+; ex 3.61
+
+(define (invert-unit-series s)
+  (cons-stream 1 (scale-stream (mul-series (stream-cdr s) (invert-unit-series s)) -1)))
+
+(display-n-elements 10 (invert-unit-series integers))
+
+; ex 3.62
+
+(define (div-series s1 s2)
+  (if (= (stream-car s2) 0)
+      (error "DENOM FIRST TERM IS ZERO" (stream-car s2))
+      (mul-series s1 (invert-unit-series s2))))
+
+(define tangent-series (div-series sine-series cosine-series))
+
+(display-n-elements 10 tangent-series)
