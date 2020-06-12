745acbf06af8f119055c0a5d5dd8361a67282fa6
Hong Wei <hongwei.zhuo@gmail.com>
2020-06-12 13:01:19 +0800

    3.5.3 WIP

--- src/chapter3/3_5_3.scm
+++ src/chapter3/3_5_3.scm
@@ -138,3 +138,101 @@
      (all-pairs (stream-cdr s) t))))
 
 (display-n-elements 10 (all-pairs integers integers))
+
+; ex 3.69
+
+(define (triples s t u)
+  (let ((t-u (pairs t u)))
+    (cons-stream
+     (cons (stream-car s) (stream-car t-u))
+     (interleave
+      (stream-map (lambda (x) (cons (stream-car s) x)) (stream-cdr t-u))
+      (triples (stream-cdr s) (stream-cdr t) (stream-cdr u))))))
+
+(display-n-elements 30 (triples integers integers integers))
+
+; ex 3.70
+
+(define (merge-weighted s1 s2 weight)
+  (cond ((stream-null? s1) s2)
+        ((stream-null? s2) s1)
+        (else
+         (let ((s1car (stream-car s1)) (s2car (stream-car s2)))
+           (cond ((< (weight s1car) (weight s2car)) (cons-stream s1car (merge-weighted (stream-cdr s1) s2 weight)))
+                 ((> (weight s1car) (weight s2car)) (cons-stream s2car (merge-weighted s1 (stream-cdr s2) weight)))
+                 (else (cons-stream s1car (merge-weighted (stream-cdr s1) s2 weight))))))))
+
+; a
+
+(define (weighted-pairs s t weight)
+  (cons-stream
+   (list (stream-car s) (stream-car t))
+   (merge-weighted
+    (stream-map (lambda (x) (list (stream-car s) x)) (stream-cdr t))
+    (weighted-pairs (stream-cdr s) (stream-cdr t) weight)
+    weight)))
+
+(display-n-elements 20 (weighted-pairs integers integers (lambda (p) (+ (car p) (cadr p)))))
+
+; b
+
+(define (custom-weight p)
+  (let ((i (car p))
+        (j (cadr p)))
+    (+ (* 2 i) (* 3 j) (* 5 i j))))
+(define (custom-filter p)
+  (let ((i (car p))
+        (j (cadr p)))
+    (not
+     (or (= (remainder i 2) 0)
+         (= (remainder i 3) 0)
+         (= (remainder i 5) 0)
+         (= (remainder j 2) 0)
+         (= (remainder j 3) 0)
+         (= (remainder j 5) 0)))))
+         
+(display-n-elements 20 (stream-filter custom-filter (weighted-pairs integers integers custom-weight)))
+
+; ex 3.71
+
+(define (cube-sum p)
+  (let ((i (car p))
+        (j (cadr p)))
+    (+ (* i i i) (* j j j))))
+
+(define cube-sum-sorted-pairs (weighted-pairs integers integers cube-sum))
+(define (current-next s)
+  (cons-stream (list (stream-car s)
+                     (stream-car (stream-cdr s)))
+               (current-next (stream-cdr s))))
+
+(define ramanujan-numbers
+  (stream-map car (stream-filter (lambda (two-sums)
+                                   (= (car two-sums) (cadr two-sums)))
+                                 (current-next (stream-map cube-sum cube-sum-sorted-pairs)))))
+
+(display-n-elements 5 ramanujan-numbers)
+
+; ex 3.72
+
+(define (square-sum p)
+  (let ((i (car p))
+        (j (cadr p)))
+    (+ (* i i) (* j j))))
+
+(define square-sum-sorted-pairs (weighted-pairs integers integers square-sum))
+(define (three-elements s)
+  (cons-stream (list (stream-car s)
+                     (stream-car (stream-cdr s))
+                     (stream-car (stream-cdr (stream-cdr s))))
+               (three-elements (stream-cdr s))))
+
+(define square-sum-in-three-ways
+  (stream-filter (lambda (three-elements)
+                   (and (= (square-sum (car three-elements))
+                           (square-sum (cadr three-elements)))
+                        (= (square-sum (cadr three-elements))
+                           (square-sum (caddr three-elements)))))
+                 (three-elements square-sum-sorted-pairs)))
+
+(display-n-elements 5 square-sum-in-three-ways)
