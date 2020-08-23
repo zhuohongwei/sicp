ecb2d96d26533c4deec7d29e3826e51b9d55fb5e
Hong Wei <hongwei.zhuo@gmail.com>
2020-08-23 10:29:53 +0800

    ex 4.21

--- src/chapter4/4_1_6.scm
+++ src/chapter4/4_1_6.scm
@@ -633,4 +633,26 @@
                      '<procedure-env>))
       (display object)))
 
-(driver-loop)
+;(driver-loop)
+
+;;; ex 4.21
+
+;;; a
+((lambda (n)
+   ((lambda (fib) (fib fib n))
+    (lambda (fb k)
+      (cond ((= k 0) 0)
+            ((= k 1) 1)
+            ((> k 1)
+             (+ (fb fb (- k 1)) (fb fb (- k 2))))))))
+ 7)
+        
+;;; b
+(define (f x)
+  ((lambda (even? odd?) (even? even? odd? x))
+   (lambda (ev? od? n)
+     (if (= n 0) true (od? ev? od? (- n 1))))
+   (lambda (ev? od? n)
+     (if (= n 0) false (ev? ev? od? (- n 1))))))
+(f 7)
+
